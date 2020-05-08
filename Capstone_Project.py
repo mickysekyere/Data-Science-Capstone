a005acf1fae3c47cd7535465d61c396275b78ae9
Your Name <you@example.com>
2020-05-08 12:41:02 +0000

    this script outlines my capstone data science projects

---
+++ Capstone_Project.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Capstone Projects
+
+# ### This notebook will be mainly used for my datascience capstone projects
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
